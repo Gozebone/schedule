a89f9eefdb225ed350fb741f97464d141ab314eb
Ismaxis <max200412345@gmail.com>
2022-09-11 21:23:22 +0300

    first work version
    html bug fixed with replaces

---
+++ main.py
@@ -0,0 +1,73 @@
+from datetime import date, timedelta
+import google_calendar.event_funcs as event_funcs
+import google_calendar.google_service as google_service
+import parse.parser as parser
+
+
+def main():
+    # 1 request to INMO to get schedule
+    # 2 create events
+    # 2 #1 check if created
+
+    # 0
+    service = google_service.get_service()
+    group = 'M3136'
+
+    # 1
+    # start of week
+    today = date.today()
+    start_of_week = today - timedelta(days=today.weekday())
+
+    week = parser.get_schedule(group)
+
+    for day in week.days:
+        cur_date = str(start_of_week + timedelta(days=day.day_index))
+        for lesson in day.lessons:
+            start = cur_date + 'T' + lesson.time_start + ':00+03:00'
+
+            end = cur_date + 'T' + lesson.time_end + ':00+03:00'
+
+            event = {
+                'summary': lesson.lesson,
+                'location': lesson.address,
+                'description': lesson.room,
+                'start': {
+                    'dateTime': start,  # '2022-09-11T12:00:00+03:00',
+                },
+                'end': {
+                    'dateTime': end,  # '2022-09-11T14:00:00+03:00',
+                },
+                'colorId': lesson.type + 1,
+            }
+            event_funcs.create_event(service, event)
+
+    # event_funcs.create_event(service, event1)
+    # event_funcs.create_event(service, event2)
+
+    start = '2022-09-11T11:00:00+03:00'
+    end = '2022-09-11T18:00:00+03:00'
+
+    events = event_funcs.get_events_by_time(service, start, end)
+    for event in events:
+        event_funcs.delete_event(service, event['id'])
+
+    # # Call the Calendar API
+    # now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    # print('Getting the upcoming 10 events')
+    # events_result = service.events().list(calendarId='primary', timeMin=now,
+    #                                       maxResults=10, singleEvents=True,
+    #                                       orderBy='startTime').execute()
+    # events = events_result.get('items', [])
+
+    # if not events:
+    #     print('No upcoming events found.')
+    #     return
+
+    # # Prints the start and name of the next 10 events
+    # for event in events:
+    #     start = event['start'].get('dateTime', event['start'].get('date'))
+    #     print(start, event['summary'])
+
+
+if __name__ == '__main__':
+    main()
--- parse/parser.py
+++ parse/parser.py
@@ -1,17 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 class Lesson:
     def __init__(self, tr):
-        self.time = self.time_parse(tr.select(".time > span")[0].get_text())
+        self.time_start, self.time_end = self.time_parse(
+            tr.select(".time > span")[0].get_text())
         self.room = self.room_parse(tr.select(".room > dl > dd")[0].get_text())
-        self.address = self.address_parse(tr.select(".room > dl > dt > span")[0].get_text())
-        self.lesson, self.type = self.lesson_parse(tr.select(".lesson > dl > dd")[0].get_text())
+        self.address = self.address_parse(
+            tr.select(".room > dl > dt > span")[0].get_text())
+        self.lesson, self.type = self.lesson_parse(
+            tr.select(".lesson > dl > dd")[0].get_text())
 
     @staticmethod
     def time_parse(time):
-        return time[:time.find('-')]
+        return time[:5], time[6:]
 
     @staticmethod
     def room_parse(room):
@@ -19,7 +23,8 @@ class Lesson:
 
     @staticmethod
     def address_parse(address):
-        return address[:address.find(' ')]
+        return address
+        # return address[:address.find(' ')]
 
     @staticmethod
     def lesson_parse(lesson):
@@ -28,11 +33,11 @@ class Lesson:
         lesson_type = lesson[open_br_index + 1:close_br_index]
         lesson_type_index = -1
 
-        if lesson_type == 'ЛЕК':
+        if lesson_type.upper() == 'ЛЕК':
             lesson_type_index = 0
-        elif lesson_type == 'ПРАК':
+        elif lesson_type.upper() == 'ПРАК':
             lesson_type_index = 1
-        elif lesson_type == 'ЛАБ':
+        elif lesson_type.upper() == 'ЛАБ':
             lesson_type_index = 2
 
         return lesson[:open_br_index], lesson_type_index
@@ -42,8 +47,7 @@ class Day:
     def __init__(self, day_schedule_html, day_index):
         self.lessons = []
         self.day_index = day_index
-        tr_array = day_schedule_html.find_all('tr')
-        tr_array.pop()
+        tr_array = day_schedule_html.select('tr')
         for current_lesson in tr_array:
             self.lessons.append(Lesson(current_lesson))
 
@@ -57,7 +61,7 @@ class Week:
 
 
 def group_number_generator(group):
-    return f'https://itmo.ru/ru/schedule/0/{group}/raspisanie_zanyatiy_{group}.htm'
+    return f'https://itmo.ru/ru/schedule/0/{group}/1/raspisanie_zanyatiy_{group}.htm'
 
 
 def schedule_by_group(group):
@@ -70,8 +74,25 @@ def schedule_by_group(group):
     }
 
     group_url = group_number_generator(group)
-    req = requests.get(group_url, headers)
-    soup = BeautifulSoup(req.content, 'html.parser')
+
+    req = requests.get(group_url)
+
+    string = req.text.replace('<tbody><th', '<tbody><tr><th')
+    string_2 = string.replace('<tr></tbody>', '</tbody>')
+
+    soup = BeautifulSoup(bytes(string_2, 'utf-8'), 'lxml')
+
+    # driver = webdriver.Chrome()
+
+    # driver.get(group_url)
+
+    # html = driver.page_source
+
+    # soup = BeautifulSoup(html)
+
+    # f = open("response.txt", "w")
+    # f.write(req.content.__str__())
+    # f.close()
 
     schedule_html = []
     for day in range(1, 7):
@@ -80,6 +101,5 @@ def schedule_by_group(group):
     return schedule_html
 
 
-group = 'M3107'
-
-Week(schedule_by_group(group))
+def get_schedule(group):
+    return Week(schedule_by_group(group))
