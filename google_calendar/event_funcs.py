c1e6a7366ac391103efad4167a2a798f9ec52487
Ismaxis <max200412345@gmail.com>
2022-09-19 00:41:33 +0300

    add separate calendar feature
    there is a bunch of details that needs to be finished 
    look form them in todo

--- google_calendar/event_funcs.py
+++ google_calendar/event_funcs.py
@@ -1,21 +1,69 @@
+from calendar import calendar
 import datetime
 
-
-def create_event(service, event):
-    event = service.events().insert(calendarId='primary', body=event).execute()
-    print('Event created: %s' % (event.get('htmlLink')))
+CALENDAR_NAME = "ITMO Schedule"
 
 
+def create_calendar(service):
+    calendar = {
+        'summary': CALENDAR_NAME,
+        'timeZone': 'Europe/Moscow'
+    }
+
+    service.calendars().insert(body=calendar).execute()
+
+
+def is_schedule_calendar_exist(service):
+    page_token = None
+    while True:
+        calendar_list = service.calendarList().list(pageToken=page_token).execute()
+        for calendar_list_entry in calendar_list['items']:
+            if calendar_list_entry['summary'] == CALENDAR_NAME:
+                return True
+            
+        page_token = calendar_list.get('nextPageToken')
+        if not page_token:
+            break
+        
+    return False
+        
+        
+
+def get_schedule_calendar(service):
+    page_token = None
+    schedule_calendar = None
+    while True:
+        calendar_list = service.calendarList().list(pageToken=page_token).execute()
+        for calendar_list_entry in calendar_list['items']:
+            if calendar_list_entry['summary'] == CALENDAR_NAME:
+                schedule_calendar = calendar_list_entry
+                break
+            
+        page_token = calendar_list.get('nextPageToken')
+        if not page_token:
+            break
+    if schedule_calendar == None:
+        print(f"No {CALENDAR_NAME} calendar found\nCheck if you have renamed it")
+        
+    return schedule_calendar
+
+
+def create_event(service, calendar, event):
+    event = service.events().insert(calendarId=calendar['id'], body=event).execute()
+    print('Event created in calendar: %s \nLink: %s' % (calendar['summary'], event.get('htmlLink')))
+
+
+# Example call: 
+#event_funcs.delete_event(service, event['id']) 
 def delete_event(service, id):
     service.events().delete(calendarId='primary',
                             eventId=id).execute()
 
 
-def get_events(service):
+def get_events(service, amount: int):
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
     events_result = service.events().list(calendarId='primary', timeMin=now,
-                                          maxResults=10, singleEvents=True,
+                                          maxResults=amount, singleEvents=True,
                                           orderBy='startTime').execute()
     return events_result.get('items', [])
 
--- main.py
+++ main.py
@@ -6,13 +6,34 @@ import parse.parser as parser
 
 def main():
     service = google_service.get_service()
-    group = input().capitalize()
 
+    # getting google calendar
+    if not event_funcs.is_schedule_calendar_exist(service):
+        print(f"You dont have a {event_funcs.CALENDAR_NAME} calendar, create it? (Y/N): ")
+        while True:
+            answer = input()
+            if answer == "Y":
+                print("Creating calendar")
+                event_funcs.create_calendar(service)
+                break
+            elif answer == "N":
+                print("Terminating...")
+                return
+            else:
+                #TODO ask if user wants to use some other calendar
+                print("Unexpected answer, try again (Y/N): ")
+                continue         
+    
+    schedule_calendar = event_funcs.get_schedule_calendar(service)
+    
+    # schedule parse
+    group = input("Enter group number in M3107 format: ").capitalize()
+    week = parser.get_schedule(group)
+    
     today = date.today()
     start_of_week = today - timedelta(days=today.weekday())
-
-    week = parser.get_schedule(group)
-
+    
+    #TODO update event if its already exist
     for day in week.days:
         cur_date = str(start_of_week + timedelta(days=day.day_index))
         for lesson in day.lessons:
@@ -31,8 +52,14 @@ def main():
                     'dateTime': end,
                 },
                 'colorId': lesson.type + 1,  # colors goes from 1 to 12
+                'reminders': {
+                    'useDefault': False,
+                    'overrides': [
+                        {'method': 'popup', 'minutes': 10},
+                    ],
+                },
             }
-            event_funcs.create_event(service, event)
+            event_funcs.create_event(service, schedule_calendar, event)
             
 if __name__ == '__main__':
     main()
