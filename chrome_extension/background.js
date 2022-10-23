aa0bfbd128db23844d14ad829c94cc8cad982853
Ismaxis <Ismaxis@github.com>
2022-10-24 00:09:21 +0300

    name and pass field added

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,14 +1,17 @@
-server_address = '194.67.111.111:8000'
+var server_address = '127.0.0.1:8000'
 
 chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		var url = 'http://' + server_address + '/google_calendar/create_events/?group='+ encodeURIComponent(request.group) ;
-		
-		fetch(url)
-		.then(response => response.json())
-		.then(response => sendResponse({farewell: response}))
-		.catch(error => console.log(error))
-			
-		return true;  // Will respond asynchronously.		  
+    function(request, sender, sendResponse) {
+        var url = 'http://' + server_address + '/google_calendar/create_events/' + 
+                            '?name=' + encodeURIComponent(request.name) +
+                            '&password=' + encodeURIComponent(request.password) +
+                            '&group=' + encodeURIComponent(request.group);
+
+        fetch(url)
+        .then(response => response.json())
+        .then(response => sendResponse({farewell: response}))
+        .catch(error => console.log(error))
+
+        return true;  // Will respond asynchronously.
     }
 );
--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,16 +1,25 @@
 $(function(){
-    $('#addSchedule').click(function(){
+    $('#addSchedule').click(function() {
+		var nickname = $('#nickname').val();
+		var password = $('#password').val();
 		var group = $('#group').val();
-		if (group.length == 5){
-            chrome.runtime.sendMessage(
-				{group: group},
+		
+		if (group.length == 5) {
+            chrome.runtime.sendMessage (
+				{
+                    name: nickname,
+                    password: password,
+                    group: group,
+				},
 				function(response) {
 					result = response.farewell;
-					alert(result.summary);					
+					alert(result.summary);				
 				}
             );
 		}
 			
+		$('#nickname').val('');		
+		$('#password').val('');		
 		$('#group').val('');		
     });
 });
