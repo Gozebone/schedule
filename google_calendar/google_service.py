6fd4b867579b79f516f39747777cca730b8a0df8
Ismaxis <Ismaxis@github.com>
2023-09-11 14:14:59 +0300

    + credential path fix

--- google_calendar/google_service.py
+++ google_calendar/google_service.py
@@ -11,9 +11,11 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-TOKEN_PATH = 'google_calendar/token.json'
-CRED_PATH = 'google_calendar/credentials.json'
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+BASE_PATH = os.path.dirname(__file__)
+TOKEN_PATH = os.path.join(BASE_PATH, "token.json")
+CRED_PATH = os.path.join(BASE_PATH, "credentials.json")
+
 
 def get_service():
     creds = None
@@ -23,25 +25,26 @@ def get_service():
     if os.path.exists(TOKEN_PATH):
         with open(TOKEN_PATH) as fp:
             json_object = json.load(fp)
-            mask = '%Y-%m-%dT%H:%M:%S.%fZ'
-            if datetime.datetime.strptime(json_object['expiry'], mask) > datetime.datetime.today():  
-                creds = Credentials.from_authorized_user_file(
-                    TOKEN_PATH, SCOPES)
+            mask = "%Y-%m-%dT%H:%M:%S.%fZ"
+            if (
+                datetime.datetime.strptime(json_object["expiry"], mask)
+                > datetime.datetime.today()
+            ):
+                creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CRED_PATH, SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(CRED_PATH, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open(TOKEN_PATH, 'w') as token:
+        with open(TOKEN_PATH, "w") as token:
             token.write(creds.to_json())
 
     try:
-        service = build('calendar', 'v3', credentials=creds)
+        service = build("calendar", "v3", credentials=creds)
         return service
 
     except HttpError as error:
-        print('An error occurred: %s' % error)
+        print("An error occurred: %s" % error)
